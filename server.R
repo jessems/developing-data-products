7cbbeda06790a234510bb94b0fd752b63ef6c776
Jesse Szepieniec <jessems@gmail.com>
2015-12-27 20:29:37 +0100

    Final commit

--- server.R
+++ server.R
@@ -1,11 +1,17 @@
 library(shiny)
 
+#min_year <- min(unique(as.numeric(names(df[,-(1:2)]))))
+
 
 shinyServer(
   function(input, output) {
-    df <- read.csv('refugees.csv', header=TRUE, check.names=FALSE, stringsAsFactors=FALSE)
+    df <- read.csv('refugees.csv', header=TRUE, check.names=FALSE, stringsAsFactors=FALSE, encoding="UTF-16")
+    if (exists("df")){
       output$newBarPlot <- renderPlot({
+        #barplot(as.numeric(df[df[,2]==input$origin,input$year_from:2013-1957]), ylab="Refugees", names.arg=names(df[df[,2]==input$origin,input$year_from:2013-1957]), angle=90)
         barplot(as.numeric(df[df[,2]==input$origin,input$year_from:2013-1957]), ylab="Refugees", names.arg=names(df[df[,2]==input$origin,input$year_from:2013-1957]), angle=90)
-      })
+        
+                })
+    }
   }
 )
--- ui.R
+++ ui.R
@@ -1,19 +1,24 @@
 library(shiny)
 
-df <- read.csv('refugees.csv', header=TRUE, check.names=FALSE, stringsAsFactors=FALSE)
+df <- read.csv('refugees.csv', header=TRUE, check.names=FALSE, stringsAsFactors=FALSE, fileEncoding="latin1")
 
 
 shinyUI(pageWithSidebar(
   headerPanel("Refugees by Origin"),
   sidebarPanel(
+    p('Welcome to my Refugee-by-Origins app!'),
+    p('I created this app to raise awareness for the current, global refugee crisis.'),
+    p('Using this app you can select a country of origin and a time period.'),
+    p('The app will show you a time distribution of the refugee count for that country during that period.'),
+    p('Note: The period end is fixed at 2013, you can only choose the begin date.'),
+    p('Data is from http://www.unhcr.org/'),
+    
              selectInput("origin", 
                          "Origin:", 
                          c("All", 
-                           unique(as.character(df$Origin)))),
-             selectInput("year_from", 
-                         "From Year:", 
-                         c(1960, 
-                           unique(as.numeric(names(df[,-(1:2)])))))
+                           unique(as.character(df$Origin))), selected=unique(as.character(df$Origin))[1]),
+             sliderInput("year_from", "Year From:",
+                         min = 1960, max = 2013, value = 1960)
   ),
   mainPanel(
     plotOutput('newBarPlot')
